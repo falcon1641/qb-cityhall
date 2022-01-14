b932db70c1cce3961fe45027f597f4ced96e18cf
falcon1641 <94806141+falcon1641@users.noreply.github.com>
2022-01-14 03:07:35 -0500

    coords change

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = Config or {}
 Config.CompanyPrice = 25000
 
 Config.Cityhall = {
-    coords = vector3(-265.0, -963.6, 31.2)
+    coords = vector3(-235.59, -920.58, 32.31)
 }
 
 Config.DriverTest = {
